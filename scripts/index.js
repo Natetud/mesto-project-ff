95c2a572304122b8c3b59dbb93d9f947cc1bd36a
Natalia Udovichenko <natetud@vk.com>
2025-03-10 20:51:41 +0500

    Попытка 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,182 +1,45 @@
-import '../pages/index.css';
-import { createCard} from "./card.js";
-import { openModal, closeModal } from "./modal.js";
-import { enableValidation, clearValidation } from "./validation";
-import { getUserData, getCards, postCard, patchUserData, deleteCardOnApi, likeCardOnApi, unlikeCardOnApi, patchUserAvatar } from "./api.js";
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// Валидация
-const validationConfig = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_visible'
-  }
-  
-  // Переменные
-  const cardTemplate = document.querySelector('#card-template').content;
-  const placesList = document.querySelector('.places__list');
-  
-  const profileTitle = document.querySelector('.profile__title');
-  const profileDescription = document.querySelector('.profile__description');
-  const profileImage = document.querySelector('.profile__image');
-  
-  const popupNewCard = document.querySelector('.popup_type_new-card');
-  const popupProfile = document.querySelector('.popup_type_edit');
-  const popupAvatar = document.querySelector('.popup_type_avatar');
-  const popupImage = document.querySelector('.popup_type_image');
-  
-  const imagePopup = document.querySelector('.popup__image');
-  const paragraphPopup = document.querySelector('.popup__caption');
-  
-  const formProfile = document.forms.editProfile;
-  const formCard = document.forms.newPlace;
-  const formAvatar = document.forms.editAvatar;
-  
-  const editButton = document.querySelector('.profile__edit-button');
-  const addButton = document.querySelector('.profile__add-button');
-  const avatarButton = document.querySelector('.profile__edit-avatar-button');
-  
-  let usrId = '';
-  
-  
-  getData();
-  
-  enableValidation(validationConfig);
-  
-  
-  editButton.addEventListener('click', () => {
-    clearValidation(formProfile, validationConfig);
-    setProfileInfo(formProfile, profileTitle, profileDescription);
-    openModal(popupProfile);
-  });
-  
-  addButton.addEventListener('click', () => {
-    clearValidation(formCard, validationConfig);
-    openModal(popupNewCard);
-  });
-  
-  avatarButton.addEventListener('click', () => {
-    clearValidation(formAvatar, validationConfig);
-    openModal(popupAvatar);
-  });
-  
-  // Обработка
-  formProfile.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    changeButtonStatus(formProfile, true);
-    patchUserData(formProfile.elements.name.value, formProfile.elements.description.value)
-      .then((res) => {
-        changeButtonStatus(formProfile, false);
-        setApiUserInfo(profileTitle, profileDescription, profileImage, res);
-        closeModal(popupProfile);
-      })
-      .catch((err) => {
-        changeButtonStatus(formProfile, false, true);
-        console.log(err);
-      });
-  });
-  
-  formCard.addEventListener('submit', (evt) => {
-    const cardInfo =
-    {
-      name: formCard.elements['place-name'].value,
-      link: formCard.elements.link.value
-    };
-  
-    evt.preventDefault();
-    changeButtonStatus(formCard, true);
-  
-    postCard(cardInfo)
-      .then((res) => {
-        cardInfo._id = res._id;
-        cardInfo.likes = res.likes;
-        changeButtonStatus(formCard, false);
-        const createdCard = createCard(cardInfo, cardTemplate, deleteCardOnApi, likeCardOnApi, unlikeCardOnApi, openCardModal, usrId);
-        placesList.prepend(createdCard);
-        closeModal(popupNewCard);
-      })
-      .catch((err) => {
-        changeButtonStatus(formCard, false, true);
-        console.log(err);
-      });
-  });
-  
-  popupAvatar.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    changeButtonStatus(formAvatar, true);
-    patchUserAvatar(formAvatar.elements.avatar.value)
-      .then(res => {
-        setApiUserInfo(profileTitle, profileDescription, profileImage, res);
-        changeButtonStatus(formAvatar, false);
-        closeModal(popupAvatar);
-      })
-      .catch((err) => {
-        changeButtonStatus(formAvatar, false, true);
-        console.log(err);
-      });
-  });
-  
-  // Функции
-  function getData() {
-    Promise.all([getUserData(), getCards()])
-      .then(([usrData, cardsData]) => {
-        usrId = usrData['_id'];
-        setApiUserInfo(profileTitle, profileDescription, profileImage, usrData);
-        initialApiCards(cardsData, usrId);
-      })
-      .catch((err) => console.log(err))
-  }
-  
-  function setProfileInfo(form, title, description) {
-    form.elements.name.value = title.textContent;
-    form.elements.description.value = description.textContent;
-  }
-  
-  function initialApiCards(apiDataList, usrId) {
-    apiDataList.forEach(item => {
-      const createdCard = createCard(item, cardTemplate, deleteCardOnApi, likeCardOnApi, unlikeCardOnApi, openCardModal, usrId);
-      placesList.append(createdCard);
+// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
+const popupImage = document.querySelector('.popup_type_image');
+const imagePopup = document.querySelector('.popup__image');
+const paragraphPopup = document.querySelector('.popup__caption');
+
+// @todo: Функция создания карточки
+function createCard(imageAttr, cardTemplate, openCardModal) {
+    const cardElement = cardTemplate.querySelector('.places__item.card').cloneNode(true);
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardImage = cardElement.querySelector('.card__image');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardTitle.textContent = imageAttr.name;
+    cardImage.src = imageAttr.link;
+    cardImage.alt = imageAttr.name;
+
+    // Удаление карточки
+    deleteButton.addEventListener('click', () => {
+        deleteCard(cardElement)
     });
-  }
-  
-  function openCardModal(imageSrc, paragraphText) {
-    setImageAttr(imageSrc, paragraphText);
-    openModal(popupImage);
-  }
-  
-  function setImageAttr(imageSrc, paragraphText) {
-    imagePopup.src = imageSrc;
-    imagePopup.alt = paragraphText;
-    paragraphPopup.textContent = paragraphText;
-  }
-  
-  function setApiUserInfo(currentTitle, currentDescription, currentAvatar, apiDataList) {
-    currentTitle.textContent = apiDataList.name;
-    currentDescription.textContent = apiDataList.about;
-    currentAvatar.style.backgroundImage = 'url(' + apiDataList.avatar + ')';
-  }
-  
-  function changeButtonStatus(form, status, error = false) {
-    const formButton = form.querySelector('.popup__button');
-  
-    if (status) {
-      if (form.name === 'newPlace') {
-        formButton.textContent = 'Создание...';
-      } else {
-        formButton.textContent = 'Сохранение...';
-      }
-    } else {
-      if (form.name === 'newPlace') {
-        formButton.textContent = 'Создать';
-      } else {
-        formButton.textContent = 'Сохранить';
-      }
-  
-      if (error) {
-        formButton.textContent = 'Ошибка';
-      }
-    }
-  }
-  
+    cardImage.addEventListener('click', () => {
+        openCardModal(cardImage.src, cardTitle.textContent);
+    })
+
+    return cardElement
+}
+
+// @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+// @todo: Вывести карточки на страницу
+function initialCardsInDOM(dataList) {
+    dataList.forEach(item => {
+        console.log({item})
+        const createdCard = createCard(item, cardTemplate, openCardModal);
+        placesList.append(createdCard);
+    });
+}
+initialCardsInDOM(initialCards)
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,39 +1,50 @@
-export function openModal(modalWindow) {
-    modalWindow.classList.add('popup_is-opened');
-  
-    modalWindow.addEventListener('click', closeByButton);
-    modalWindow.addEventListener('click', closeByOverlay);
-    document.addEventListener('keydown', closeByEscape);
-  }
-  
-  export function closeModal(modalWindow) {
-    modalWindow.classList.remove('popup_is-opened');
-  
-    modalWindow.removeEventListener('click', closeByButton);
-    modalWindow.removeEventListener('click', closeByOverlay);
-    document.removeEventListener('keydown', closeByEscape);
-  }
-  
-  function closeByButton(evt) {
-    if (evt.target.classList.contains('popup__close')) {
+function closeByButton(evt) {
+  if (evt.target.classList.contains('popup__close')) {
       closeModal(getActiveModal());
-    }
   }
-  
-  function closeByOverlay(evt) {
-    if (evt.target.classList.contains('popup')) {
+}
+function getActiveModal() {
+  return document.querySelector('.popup_is-opened');
+}
+function closeByOverlay(evt) {
+  if (evt.target.classList.contains('popup')) {
       closeModal(getActiveModal());
-    }
   }
-  
-  function closeByEscape(evt) {
-    if (evt.key === 'Escape') {
+}
+function closeByEscape(evt) {
+  if (evt.key === 'Escape') {
       closeModal(getActiveModal())
-    }
   }
-  
-  function getActiveModal() {
-    const activeModal = document.querySelector('.popup_is-opened');
-    return activeModal;
+}
+
+function openModal(modalWindow) {
+
+  modalWindow.classList.add('popup_is-opened');
+
+  modalWindow.addEventListener('click', closeByButton);
+  modalWindow.addEventListener('click', closeByOverlay);
+  document.addEventListener('keydown', closeByEscape);
+}
+
+// Закрытие модального окна
+function closeModal(modalWindow) {
+
+
+  modalWindow.classList.remove('popup_is-opened');
+
+  modalWindow.removeEventListener('click', closeByButton);
+  modalWindow.removeEventListener('click', closeByOverlay);
+  document.removeEventListener('keydown', closeByEscape);
+}
+
+// Открытие модального окна с параметрами: картинка, текст
+function openCardModal(imageSrc, paragraphText) {
+  function setImageAttr(imageSrc, paragraphText) {
+      imagePopup.src = imageSrc;
+      imagePopup.alt = paragraphText;
+      paragraphPopup.textContent = paragraphText;
   }
-  
+
+  setImageAttr(imageSrc, paragraphText);
+  openModal(popupImage);
+}
